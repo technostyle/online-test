aed76cba7103d654b8e3077a4e8bef9b45d3ec2d
danila <dm.perlov@jet.su>
2020-07-16 19:29:20 +0300

    initial set-up

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    context: __dirname,
+    entry: './app/index.jsx',
+    output: {
+        path: __dirname + '/dist/',
+        filename: 'bundle.js',
+    },
+    devtool: '#sourcemap',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { modules: true } },
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+    devServer: {
+        contentBase: __dirname,
+        compress: true,
+        port: 9000,
+    },
+    resolve: {
+        modules: ['app', 'node_modules'],
+        extensions: ['.js', '.jsx'],
+    },
+};
